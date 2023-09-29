3e149190136809bd836aee48f30ba98743fc4d22
D.Rajani <rajanidharmaram03@gmail.com>
2023-09-29 23:54:04 +0530

    Refactored code using Environment Variables

--- app.py
+++ app.py
@@ -4,34 +4,51 @@ import os
 import glob
 import pandas as pd
 
+# Some Issues to be addressed are:
+
+# Folder paths are hard coded.
+# Schemas.json path is also hard coded.
+# Modularization with reusability.
+
+
 def get_columns(ds):
-        with open('data/retail_db/schemas.json') as fp:
-            schemas = json.load(fp)
-        try:
-            schema = schemas.get(ds)
-            if not schema:
-                raise KeyError
-            cols = sorted(schema, key=lambda s: s['column_position'])
-            columns = [col['column_name'] for col in cols]
-            return columns
-        except KeyError:
-            print(f'Schema not found for {ds}')
-            return
+    schemas_file_path=os.environ.setdefault('SCHEMAS_FILE_PATH', 'data/retail_db/schemas.json')
+    with open(schemas_file_path) as fp:
+        schemas = json.load(fp)
+    try:
+        schema = schemas.get(ds)
+        if not schema:
+            raise KeyError
+        cols = sorted(schema, key=lambda s: s['column_position'])
+        columns = [col['column_name'] for col in cols]
+        return columns
+    except KeyError:
+        print(f'Schema not found for {ds}')
+        return
+
 
 def main():
-    for path in glob.glob('data/retail_db/*'):
+    src_base_dir=os.environ['SRC_BASE_DIR']
+    tgt_base_dir=os.environ['TGT_BASE_DIR']
+    for path in glob.glob(f'{src_base_dir}/*'):
         if os.path.isdir(path):
             ds = os.path.split(path)[1]
             for file in glob.glob(f'{path}/part*'):
                 df = pd.read_csv(file, names=get_columns(ds))
-                os.makedirs(f'data/retail_demo/{ds}', exist_ok=True)
+                os.makedirs(f'{tgt_base_dir}/{ds}', exist_ok=True)
                 df.to_json(
-                    f'data/retail_demo/{ds}/{str(uuid.uuid1())}.json',
+                    f'{tgt_base_dir}/{ds}/{str(uuid.uuid1())}.json',
                     orient='records',
                     lines=True
                 )
-                print(f'Number of records processed for {os.path.split(file)[1]} in {ds} is {df.shape[0]}')
+                print(
+                    f'Number of records processed for {os.path.split(file)[1]} in {ds} is {df.shape[0]}')
 
 
 if __name__ == '__main__':
     main()
+
+# Exercise: Similar program file for nyse-converter
+# update app.py with core logic of File-Format-Conversion for nyse data
+# Make sure to validate to see if it is working as per the expectations or not
+# Changes are committed and pushed to remote repository
